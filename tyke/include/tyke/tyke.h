108d156cd78c0b3c63bcff2d774844673154b748
cppbugboy <cppbugboy@gmail.com>
2026-04-25 18:47:28 +0800

    优化代码

--- tyke/include/tyke/tyke.h
+++ tyke/include/tyke/tyke.h
@@ -8,8 +8,8 @@
 
 #pragma once
 
-#include "../core/request_controller.h"
-#include "../core/response_controller.h"
+#include "core/request_controller.h"
+#include "core/response_controller.h"
 #include "common/log_def.h"
 #include "component/singleton.h"
 #include "core/framework.h"
@@ -17,6 +17,6 @@
 #include "core/request_router.h"
 #include "core/response.h"
 #include "core/response_router.h"
-#include "define/request_filter.h"
-#include "define/response_filter.h"
+#include "core/request_filter.h"
+#include "core/response_filter.h"
 #include "ipc/connection_pool_factory.h"
